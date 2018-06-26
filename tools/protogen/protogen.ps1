cbe034eb3100632f0729d17cc76a4a760a986a9c
dejanstojanovic <d.stojanovic@hotmail.com>
2018-06-26 16:44:42 +0400

    Using powershell

--- tools/protogen/protogen.ps1
+++ tools/protogen/protogen.ps1
@@ -1,18 +1,32 @@
-Param(    
+Param(
 [Parameter(Mandatory=$true)]    
-[string]$protoFolder
+$protogenPath,
 [Parameter(Mandatory=$true)]    
-[string]$modelFolder
+[string]$protoFolder,
+[Parameter(Mandatory=$true)]    
+[string]$modelFolder,
 [Parameter(Mandatory=$true)]    
 [string]$namespace
  )
  
+  
+#$protogenPath = "D:\Github\dotnet-core-protogen\tools\protogen\protogen.dll"
+#$protoFolder = "D:\Github\dotnet-core-protogen\Protobuf.Autogenerate.Sample\Protos"
+#$modelFolder = "D:\Github\dotnet-core-protogen\Protobuf.Autogenerate.Sample\Models"
+#$namespace = "Protobuf.Autogenerate.Sample.Models"
 
 $protoFiles = get-childitem $protoFolder -recurse -force -include *.proto   
-foreach($protoFile in $protoFiles){     
+foreach($protoFile in $protoFiles){
 
-	Write-Output $protoFile
+    $outFilePath = $modelFolder + "\" + $protoFile.Name
 
-	#exec "dotnet protogen.dll " + $protoFile.Name + " --proto_path=" + $protoFolder + " --csharp_out="+$modelFolder+" --package=" + $namespace +"Models"
+    if(!Test-Path($outFilePath)){
+
+        $protoPathParam = "--proto_path=" + $protoFolder
+        $csharpOutParam = "--csharp_out=" + $modelFolder
+        $packageParam = "--package=" + $namespace +".Models"
+
+	    Start-Process -FilePath "dotnet" -ArgumentList $protogenPath , $protoFile.Name, $protoPathParam , $csharpOutParam , $packageParam
+    }
 
 }
